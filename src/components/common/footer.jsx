3c62e0d94932199f0b27bc827396ea512ad75b40
KUSH022 <129709416+KUSH022@users.noreply.github.com>
2024-04-04 19:15:58 +0530

    Update footer.jsx

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
 
 				<div className="footer-credits">
 					<div className="footer-credits-text">
-						© 2023 Kush Patel. All Rights Reserved.
+						© 2024 Kush Patel. All Rights Reserved.
 					</div>
 				</div>
 			</div>
