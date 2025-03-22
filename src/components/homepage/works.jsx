bc5e5938a80108d1ae10be8958f39462ff00a788
KUSH022 <129709416+KUSH022@users.noreply.github.com>
2025-03-22 19:00:51 +0530

    Update works tcs image

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,7 +15,7 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="https://en.m.wikipedia.org/wiki/File:Tata_Consultancy_Services_Logo.svg"
+								src="./download.jpg"
 								alt="."
 								className="work-image"
 							/>
